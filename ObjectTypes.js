3eb808812ea39b8d286c13400002b2e1a4b0702e
Muhammad Abdullah <muhmmadabdullah6346@gmail.com>
2024-08-01 12:20:38 +0500

    feat: Completed Object types
    I have completed object types , type aliases , optioonal types  and
    readonly . also learned some exceptions of object types

--- ObjectTypes.js
+++ ObjectTypes.js
@@ -1,8 +1,21 @@
+"use strict";
+exports.__esModule = true;
 var User = {
     name: "gladdy",
     email: "New@gmail.com",
     isActive: true
 };
-var createuser = function (name, isPaid) {
+// const createuser = ({ name: string, isPaid: boolean }) => {
+//   return `Name is ${name} and is paid is ${isPaid}`;
+// };
+var createuser = function (_a) {
+    var name = _a.name, isPaid = _a.isPaid;
+    return "Name is ".concat(name, " and is paid is ").concat(isPaid);
 };
-createuser("gladdy", true);
+console.log(createuser({ name: "gladdy", isPaid: true }));
+function obj(_a) {
+    var name = _a.name, ispaid = _a.ispaid;
+    return "Name is ".concat(name, " and is paid is ").concat(ispaid);
+}
+var y = obj({ name: "gladdy", ispaid: true });
+console.log(y);
--- ObjectTypes.ts
+++ ObjectTypes.ts
@@ -7,9 +7,67 @@ const User = {
 //   return `Name is ${name} and is paid is ${isPaid}`;
 // };
 
-const createuser= ({name,isPaid}:{name:string,isPaid:boolean}) => {
-    return `Name is ${name} and is paid is ${isPaid}`;
+const createuser = ({ name, isPaid }: { name: string; isPaid: boolean }) => {
+  return `Name is ${name} and is paid is ${isPaid}`;
 };
 console.log(createuser({ name: "gladdy", isPaid: true }));
+function obj({ name, ispaid }: { name: string; ispaid: boolean }) {
+  return `Name is ${name} and is paid is ${ispaid}`;
+}
+let y = obj({ name: "gladdy", ispaid: true });
+console.log(y);
+// Bad Objects Behaviour
+const user = (userdata: { name: string; email: string }) => {
+  return `Your user is added`;
+};
+let newUser = { name: "gladdy", email: "coding" };
+user(newUser);
+// modern optional property
+function modern_obj(obj:{firstname:string,lastname?:string}) {  //? optional
+  console.log(obj.lastname?.toUpperCase())
+}
+modern_obj({firstname:"umar",lastname:"kahalid"})
 
+// Type Aliases
+
+type User = {
+  name: string;
+  email: string;
+  address: string;
+  isActive: boolean;
+};
+type bol=boolean
+function Createuser(user:User) {
+  console.log(user.name)
+}
+type point={
+  x:number
+  y:number
+}
+function Pointcoordinate(point:point) {
+  console.log("The x coorinate is "+point.x)
+  console.log("The y coorinate is "+point.y)
+
+}
+Pointcoordinate({x:5,y:7})
+Createuser({name:"gladdy",email:"err",address:"",isActive:true})
+READONLY objections bad behaviour 
+type User2 = {
+  readonly _id:[number,string];
+  name: string;
+  address: string;
+  isActive: boolean;
+  email: string;
+};
+
+let newuser: User2 = {
+  _id: [5,"gladdy"],
+  name: "gladdy",
+  address: "Not Available",
+  isActive: true,
+  email: "g@G.com",
+};
+newuser._id[0]=7;
+
+console.log(newuser._id[0])
 export {};
