0bb576f1b78055a11a1a963f43b8145fd207df83
Muhammad Abdullah <muhmmadabdullah6346@gmail.com>
2024-08-05 11:40:23 +0500

    feat: Enums Completed

---
+++ TuplesandEnums.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+// const user: (string | number)[] = [1, "ali"];
+// tuples we cannot change order use in pi calls
+var user = ["gladdy", 9, true];
+var rgb; //you can use readonly
+rgb = [255, 255, 255];
+rgb[0] = 2;
+console.log(rgb);
+var firstusr = [21, "h@fgmail.com"];
+firstusr[1] = "ajda";
+firstusr.push(1);
+console.log(firstusr);
+var myseat = "window" /* Getseat.Window */;
+console.log(myseat);
--- TuplesandEnums.ts
+++ TuplesandEnums.ts
@@ -1,13 +1,32 @@
 // const user: (string | number)[] = [1, "ali"];
 // tuples we cannot change order use in pi calls
 const user: [string, number, boolean] = ["gladdy", 9, true];
-let rgb: [number, number, number];
+let rgb: [number, number, number]; //you can use readonly
 rgb = [255, 255, 255];
-rgb[0]=2
-console.log(rgb)
-// Bad behaviour 
+rgb[0] = 2;
+console.log(rgb);
+// Bad behaviour
 type User = [number, string];
 const firstusr: User = [21, "h@fgmail.com"];
 firstusr[1] = "ajda";
-firstusr.push(1)
-console.log(firstusr)
+firstusr.push(1);
+console.log(firstusr);
+
+// Why Enums 
+// const Asile = 0;
+// const middle = 1;
+// const window = 2;
+
+// if (seat === Asile) {
+    
+// }
+// It will generate a lot of code 
+const enum Getseat{
+ Asile="asitle",
+ Middle="middle",
+ Window="window"
+}
+const myseat=Getseat.Window
+console.log(myseat)
+
+export{}
